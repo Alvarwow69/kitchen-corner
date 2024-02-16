5ff17a12daebd12f0f59477b4fc86e6d6a287a44
Guillaume <guillaume.soisson.69@gmail.com>
2024-02-16 15:55:07 +0100

    Add timer + score

---
+++ Script/ScoreManager.cs
@@ -0,0 +1,30 @@
+using Godot;
+using System;
+
+public partial class ScoreManager : Control
+{
+	[Export] private Label _text;
+	private int _score = 0;
+
+	public override void _Ready()
+	{
+		_text.Text = "Score: " + _score;
+	}
+
+	public int GetScore()
+	{
+		return _score;
+	}
+
+	public void AddScore(int bonus)
+	{
+		_score += bonus;
+		_text.Text = "Score: " + _score;
+	}
+	
+	public void RemoveScore(int malus)
+	{
+		_score -= malus;
+		_text.Text = "Score: " + _score;
+	}
+}
