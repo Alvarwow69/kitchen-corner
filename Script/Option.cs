77af7d0d588857c5e23110dce2dab24d424021ef
Guilaume S <guillaume.soisson.69@gmail.com>
2024-03-14 15:55:44 +0100

    Add command tab

--- Script/Option.cs
+++ Script/Option.cs
@@ -4,6 +4,11 @@ using System.Diagnostics;
 
 public partial class Option : Control
 {
+	[Export] private Control _optionMenu;
+	[Export] private Control _commandMenu;
+
+	private bool _isInCommand = false;
+
 	public void OnSliderMusicChange(float value)
 	{
 		var bus = AudioServer.GetBusIndex("Music");
@@ -20,5 +25,20 @@ public partial class Option : Control
 			AudioServer.SetBusVolumeDb(bus, -80);
 		else
 			AudioServer.SetBusVolumeDb(bus, value);
+	}
+
+	public void OnButtonCommandPressed()
+	{
+		_isInCommand = !_isInCommand;
+		if (_isInCommand)
+		{
+			_optionMenu.Visible = false;
+			_commandMenu.Visible = true;
+		}
+		else
+		{
+			_optionMenu.Visible = true;
+			_commandMenu.Visible = false;
+		}
 	}
 }
